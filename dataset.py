6ff42ac16acf08963322cc69f994ba7dfcbf974e
jmcrego <jmcrego@gmail.com>
2018-12-14 10:57:30 +0100

    mode agg

--- dataset.py
+++ dataset.py
@@ -118,7 +118,7 @@ class Dataset():
             if firstline:
                 if len(line.split('\t'))==3: self.annotated = True
                 firstline = False
-            self.data.append(line.strip())
+            self.data.append(line.strip('\n'))
             self.length += 1
         f.close()
 
