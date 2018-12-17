01f5c018c555ea31415a9875ca20b920b12319a0
jmcrego <jmcrego@gmail.com>
2018-12-17 15:17:05 +0100

    -sim

--- model.py
+++ model.py
@@ -355,8 +355,15 @@ class Model():
 
             for i_sent in range(len(align_batch)):
                 n_sents += 1
-                n_unk_src = np.sum([s==0 for s in src_batch[i_sent]])
-                n_unk_tgt = np.sum([s==0 for t in tgt_batch[i_sent]])
+                s_unks = []
+                t_unks = []
+                for s in range(src_batch[i_sent]):
+                    if src_batch[i]==0:
+                        s_unks.append(raw_src_batch[i_sent][s])
+                for t in range(tgt_batch[i_sent]):
+                    if tgt_batch[i]==0:
+                        t_unks.append(raw_tgt_batch[i_sent][t])
+
                 v = Visualize(n_sents,raw_src_batch[i_sent],raw_tgt_batch[i_sent],sim_batch[i_sent],align_batch[i_sent],aggr_src_batch[i_sent],aggr_tgt_batch[i_sent],snt_src_batch[i_sent],snt_tgt_batch[i_sent])
                 print("sunk={} tunk={}".format(n_unk_src,n_unk_tgt))
                 if self.config.show_svg: v.print_svg()
